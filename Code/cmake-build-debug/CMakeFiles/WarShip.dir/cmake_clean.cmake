5905de3bd44fef0f7e55aacc2779affb3d944f8d
CUANY Simon <Simon.CUANY@cpnv.ch>
2019-03-07 16:11:56 +0100

    Code
    Code Clion

---
+++ Code/cmake-build-debug/CMakeFiles/WarShip.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WarShip.dir/main.c.obj"
+  "WarShip.pdb"
+  "WarShip.exe"
+  "WarShip.exe.manifest"
+  "libWarShip.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/WarShip.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
