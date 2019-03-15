2d201d5bf1fe8cf271f1b0b415bc4600cda99ec8
CUANY Simon <Simon.CUANY@cpnv.ch>
2019-03-15 12:14:58 +0100

    Update

--- Programme/main.c
+++ Programme/main.c
@@ -11,39 +11,63 @@
 #define SHBB 193 // ┴, Single Horizontal Bottom Border
 #define SHTB 194 // ┬, Single Horizontal Top Border
 #define SC   197 // ┼, Single Center
+é
+
+int TopBorder(void) {
+    printf ("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", STLC, SHSB, SHSB, SHSB, SHTB, SHSB, SHSB, SHSB, SHTB, SHSB,
+            SHSB, SHSB, SHTB, SHSB, SHSB, SHSB, STRC);
+
+}
+
 int main() {
     int choix;
-    int aide;
-    int jouer ;
-    printf ("\nBIENVENUE DANS LE JEU : BATAILLE NAVALE\n\n");
-
+    printf ("\n----BIENVENUE DANS LE JEU BATAILLE NAVALE----\n\n");
+printf("                   __/___            \n"
+       "             _____/______|           \n"
+       "     _______/_____\\_______\\_____     \n"
+       "     \\              < < <       |    \n"
+       "   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
     printf ("1. Jouer\n");
     printf ("2. aide\n");
     printf ("3. credits\n");
-    printf ("4. perso\n");
+    printf ("4. personnalisation\n");
     printf ("5. Quitter\n");
-    scanf ("%d",&choix);
-    printf ("Vous avez choisit l'option %d",choix);
+    scanf ("%d", &choix);
+    printf ("Vous avez choisit l'option %d", choix);
 
-    if (choix == 2)
+    if (choix == 1)     //pour pouvoir jouer
+
     {
-        printf ("\nLes regles sont simples, nous avons 3 bateaux et l'adversaire en a aussi 3.\nPour tirer rien de plus simple ; nous annoncons une case (B5) si il y a une partie du bateau en case B5, le bateau est touche. Le but est de couler les trois bateaux adverses");
-    }
-    if (choix == 1)
-
-    {   SetConsoleOutputCP(65001); // For accented characters
+        SetConsoleOutputCP (65001); // For accented characters
         printf ("\nUne grille basée sur les lignes simples:\n\n");
-        SetConsoleOutputCP(437); // For semi-graphic characters
-        printf ("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",STLC,SHSB,SHSB,SHSB,SHTB,SHSB,SHSB,SHSB,SHTB,SHSB,SHSB,SHSB,SHTB,SHSB,SHSB,SHSB,STRC);
-        printf ("%c   %c   %c   %c   %c\n",SVSB,SVSB,SVSB,SVSB,SVSB);
-        printf ("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",SVLB,SHSB,SHSB,SHSB,SC,SHSB,SHSB,SHSB,SC,SHSB,SHSB,SHSB,SC,SHSB,SHSB,SHSB,SVRB);
-        printf ("%c   %c   %c   %c   %c\n",SVSB,SVSB,SVSB,SVSB,SVSB);
-        printf ("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",SVLB,SHSB,SHSB,SHSB,SC,SHSB,SHSB,SHSB,SC,SHSB,SHSB,SHSB,SC,SHSB,SHSB,SHSB,SVRB);
-        printf ("%c   %c   %c   %c   %c\n",SVSB,SVSB,SVSB,SVSB,SVSB);
-        printf ("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",SVLB,SHSB,SHSB,SHSB,SC,SHSB,SHSB,SHSB,SC,SHSB,SHSB,SHSB,SC,SHSB,SHSB,SHSB,SVRB);
-        printf ("%c   %c   %c   %c   %c\n",SVSB,SVSB,SVSB,SVSB,SVSB);
-        printf ("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",SBLC,SHSB,SHSB,SHSB,SHBB,SHSB,SHSB,SHSB,SHBB,SHSB,SHSB,SHSB,SHBB,SHSB,SHSB,SHSB,SBRC);
+        SetConsoleOutputCP (437); // For semi-graphic characters
+        printf ("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", STLC, SHSB, SHSB, SHSB, SHTB, SHSB, SHSB, SHSB, SHTB, SHSB,
+                SHSB, SHSB, SHTB, SHSB, SHSB, SHSB, STRC);
+        printf ("%c   %c   %c   %c   %c\n", SVSB, SVSB, SVSB, SVSB, SVSB);
+        printf ("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", SVLB, SHSB, SHSB, SHSB, SC, SHSB, SHSB, SHSB, SC, SHSB, SHSB,
+                SHSB, SC, SHSB, SHSB, SHSB, SVRB);
+        printf ("%c   %c   %c   %c   %c\n", SVSB, SVSB, SVSB, SVSB, SVSB);
+        printf ("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", SVLB, SHSB, SHSB, SHSB, SC, SHSB, SHSB, SHSB, SC, SHSB, SHSB,
+                SHSB, SC, SHSB, SHSB, SHSB, SVRB);
+        printf ("%c   %c   %c   %c   %c\n", SVSB, SVSB, SVSB, SVSB, SVSB);
+        printf ("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", SVLB, SHSB, SHSB, SHSB, SC, SHSB, SHSB, SHSB, SC, SHSB, SHSB,
+                SHSB, SC, SHSB, SHSB, SHSB, SVRB);
+        printf ("%c   %c   %c   %c   %c\n", SVSB, SVSB, SVSB, SVSB, SVSB);
+        printf ("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", SBLC, SHSB, SHSB, SHSB, SHBB, SHSB, SHSB, SHSB, SHBB, SHSB,
+                SHSB, SHSB, SHBB, SHSB, SHSB, SHSB, SBRC);
     }
 
-    return 0;
+
+
+        if (choix == 2)     // pour afficher l'aide
+        {
+            printf ("\n\nLes regles sont simples, nous avons 3 bateaux et l'adversaire en a aussi 3.\nPour tirer rien de plus simple ; nous annoncons une case (B5) si il y a une partie du bateau en case B5, le bateau est touche. Le but est de couler les trois bateaux adverses");
+        }
+
+        if (choix == 3)     //pour afficher les crédits
+        {
+            printf ("\n\nCe programme a ete code par Simon Cuany pour un projet afin de s'entrainer avec le language C au CPNV de Ste-Croix.\n");
+        }
+
+        return 0;
 }
